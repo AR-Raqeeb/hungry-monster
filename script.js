cdf3e8c18f34e149460f75c33cef3683d9907026
AR-Raqeeb <arraqeeb628@gmail.com>
2021-02-08 23:15:28 +0600

    some bonus feature added

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
+// Getting all the necessary elements by their id
 const searchBox = document.getElementById('search-box');
 const searchButton = document.getElementById('search-button')
 const foodImage = document.getElementById('food-image');
 const foodName = document.getElementById('food-name');
 const ingredientTitle = document.getElementById('ingredient-header');
 const ingredientsList = document.getElementById('ingredients-list');
-const ingredient1 = document.getElementById('ingredient1');
-const ingredient2 = document.getElementById('ingredient2');
-const ingredient3 = document.getElementById('ingredient3');
-const ingredient4 = document.getElementById('ingredient4');
-const ingredient5 = document.getElementById('ingredient5');
-
+// A global variable to store all data from API
 let allData;
+// adding event-handler in the search button
 searchButton.addEventListener('click', function() {
     const foodName = searchBox.value;
+    // making the previous food items invisible when a new search occurs
     const foodItems = document.getElementById('food-list');
+    foodItems.innerHTML = '';
+    // making the previous ingredient section invisible when a new search occurs
+    const ingredientDiv = document.getElementById("ingredient");
+    ingredientDiv.style.display = 'none';
+    // keeping error message invisible until needed
+    document.getElementById("error-message").innerHTML = '';
+    // fetching data from API and converting them into JSON
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(response => response.json())
         .then(data => {
+
             allData = data.meals;
-            data.meals.map(x => {
+            data.meals.map(allFood => {
                 // Creating div to show the food matched with searched keyword
                 const div = document.createElement("div");
                 div.className = "col-md-4 food-cart";
-                div.id = `${x.idMeal}`;
-
-                div.onclick = () => foodIngredient(x.idMeal);
+                div.id = `${allFood.idMeal}`;
+                // Enabling onclick functionality for every food card separately
+                div.onclick = () => foodIngredient(allFood.idMeal);
                 const foodName = document.createElement("h4");
                 const foodImage = document.createElement('img');
-                foodImage.src = `${x.strMealThumb}`;
-                const captureFoodName = document.createTextNode(`${x.strMeal}`);
-
+                foodImage.src = `${allFood.strMealThumb}`;
+                const captureFoodName = document.createTextNode(`${allFood.strMeal}`);
+                // Appending child into their parent nodes
                 foodName.appendChild(captureFoodName);
                 div.appendChild(foodImage);
                 div.appendChild(foodName);
@@ -37,22 +43,40 @@ searchButton.addEventListener('click', function() {
                 return foodItems.appendChild(div);
             })
         })
+        // Catching error when the keyword is not matched with the database
+        .catch((error) =>
+            errorMessage("Sorry! No result found...Please search something else.")
+        );
     searchBox.value = '';
 })
+
+// Error message function
+const errorMessage = error => {
+    const errorAlert = document.getElementById("error-message");
+    errorAlert.innerText = error;
+}
+
+// Working with the food ingredients section that appears when clicked on a food card
 const foodIngredient = id => {
+    const ingredientDiv = document.getElementById("ingredient");
+    ingredientDiv.style.display = 'block';
     ingredientsList.style.display = 'block';
     foodImage.style.display = 'block';
-    allData.find(x => {
-        if (x.idMeal === id) {
-            foodImage.src = x.strMealThumb;
-            foodName.innerText = x.strMeal;
-            ingredientTitle.innerText = 'Ingredients';
 
-            ingredient1.innerText = x.strIngredient1;
-            ingredient2.innerText = x.strIngredient2;
-            ingredient3.innerText = x.strIngredient3;
-            ingredient4.innerText = x.strIngredient4;
-            ingredient5.innerText = x.strIngredient5;
+    allData.find(allFood => {
+        if (allFood.idMeal === id) {
+            // Assigning data into the image and header tags
+            foodImage.src = allFood.strMealThumb;
+            foodName.innerText = allFood.strMeal;
+            ingredientTitle.innerText = 'Ingredients';
+            // showing the ingredients for a particular food item
+            ingredient1.innerText = allFood.strIngredient1;
+            ingredient2.innerText = allFood.strIngredient2;
+            ingredient3.innerText = allFood.strIngredient3;
+            ingredient4.innerText = allFood.strIngredient4;
+            ingredient5.innerText = allFood.strIngredient5;
+            ingredient6.innerText = allFood.strIngredient6;
+            ingredient7.innerText = allFood.strIngredient7;
         }
     })
 }
